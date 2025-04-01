845cbe0187db709f489eeffd6223784f45009009
LuisC-web <72262647+LuisC-web@users.noreply.github.com>
2025-04-01 16:47:29 -0500

    Agregando segundo LED

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from machine import Pin
 import time
 
 led1 = Pin(15, Pin.OUT)
-
+led2 = Pin(14, Pin.OUT)
 while True:
     led1.on()
+    led2.off()
+
     time.sleep(1)
     led1.off()
+    led2.on()
     time.sleep(1)
